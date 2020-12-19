241831acfbfb8976b03c794f8beee4bb56ed67b9
buzaevfedor <fbuzaev@gmail.com>
2020-12-20 01:21:54 +0300

    commit for second week

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
 import flask
 from flask import Flask
-from data import tours
+from data import tours, title, subtitle, description, departures
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
-    return flask.render_template("index.html")
+    return flask.render_template("index.html",
+                                 tours=tours,
+                                 title=title,
+                                 subtitle=subtitle,
+                                 description=description,
+                                 departures=departures
+                                 )
 
 
 @app.route("/departures/<departure>/")
-def render_departures():
-    return flask.render_template("departure.html")
+def render_departures(departure):
+    tour_items = dict(filter(lambda tour: tour[1]["departure"] == departure, tours.items()))
+    return flask.render_template("departure.html",
+                                 subtitle=subtitle,
+                                 title=title,
+                                 departure=departure,
+                                 tours=tour_items,
+                                 departures=departures
+                                 )
 
 
 @app.route("/tours/<id>/")
-def render_tours():
-    return flask.render_template("tour.html")
+def render_tours(id):
+    id = int(id)
+    return flask.render_template("tour.html",
+                                 subtitle=subtitle,
+                                 title=title,
+                                 tour=tours[id],
+                                 departures=departures
+                                 )
 
 
 @app.errorhandler(404)
@@ -30,29 +49,29 @@ def render_server_error(error):
     return "Что-то не так, но мы все починим"
 
 
-# Бонусная часть
-@app.route("/data/")
-def data():
-    return flask.render_template("data.html",
-                                 tours=tours
-                                 )
-
-
-@app.route("/data/departures/<departure>")
-def get_departure(departure):
-    return flask.render_template("data_departure.html",
-                                 tours=tours,
-                                 departure=departure
-                                 )
-
-
-@app.route("/data/departures/<id>/")
-def get_id(id):
-    return flask.render_template("data_id.html",
-                                 tours=tours,
-                                 id2=int(id)
-                                 )
+# # Бонусная часть
+# @app.route("/data/")
+# def data():
+#     return flask.render_template("data.html",
+#                                  tours=tours
+#                                  )
+#
+#
+# @app.route("/data/departures/<departure>")
+# def get_departure(departure):
+#     return flask.render_template("data_departure.html",
+#                                  tours=tours,
+#                                  departure=departure
+#                                  )
+#
+#
+# @app.route("/data/departures/<id>/")
+# def get_id(id):
+#     return flask.render_template("data_id.html",
+#                                  tours=tours,
+#                                  id2=int(id)
+#                                  )
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
